73035271d4f0c7dac436acbd096e56be36016df1
Tarek Shahin <tchahin1@etf.unsa.ba>
2019-03-06 09:47:25 +0100

    Reviews

--- src/main/java/com/example/restaurants/controllers/ReviewsController.java
+++ src/main/java/com/example/restaurants/controllers/ReviewsController.java
@@ -4,6 +4,7 @@ import com.example.restaurants.data.models.Review;
 import com.example.restaurants.repositories.ReviewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,5 +23,12 @@ public class ReviewsController {
         Review save = reviewsRepository.save(review);
 
         return new ResponseEntity("New review successfully added.", HttpStatus.OK);
+    }
+
+    @GetMapping("/check")
+    public Review check(String restaurant, String user){
+        Review review = reviewsRepository.findReviewByRestaurant_NameAndUser_Email(restaurant, user);
+        if(review!=null) return review;
+        return review;
     }
 }
--- src/main/java/com/example/restaurants/repositories/ReviewsRepository.java
+++ src/main/java/com/example/restaurants/repositories/ReviewsRepository.java
@@ -4,4 +4,5 @@ import com.example.restaurants.data.models.Review;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewsRepository extends CrudRepository<Review, Long> {
+    Review findReviewByRestaurant_NameAndUser_Email(String restaurant, String user);
 }
